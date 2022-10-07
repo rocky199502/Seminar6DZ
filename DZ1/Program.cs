40f9ab074b225b98e5265c14524ce5c3f01b322c
Anastasia <nastys11@gmail.com>
2022-10-07 17:26:04 +0300

    Initial commit

---
+++ DZ1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+namespace DZ1
+{
+    class Program
+    {
+        static int[] vvodMas(int size)
+        {
+            Console.WriteLine("Введите числа через пробел и нажмите Enter: ");
+            string[] massiv =Console.ReadLine().Split();
+            int[] massivInt = new int[size];
+            for (int i =0;i<massiv.Length;i++)
+            {
+                massivInt[i]=int.Parse(massiv[i]);
+            }
+            return massivInt; 
+        }
+        static int countMas(int[] mass)
+        {
+            int count = 0;
+            for (int i =0;i<mass.Length;i++)
+            {
+                if (mass[i]>0)
+                {
+                    count++;
+                }
+            }
+            return count;
+            }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какое число элементов вы хотите ввести? ");
+            int size = int.Parse(Console.ReadLine()); 
+            int[] mass = vvodMas(size);
+            Console.Write("Полученный массив [  ");
+            for (int i =0; i<mass.Length; i++)
+            {
+                Console.Write(mass[i]+"  ");
+            }
+            Console.WriteLine("]"); 
+            int count = countMas(mass);
+            Console.WriteLine(" ");
+            Console.WriteLine("Колличество неотрицательных значений: "+count);
+        }
+    }
+}
